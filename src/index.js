5a9a2616ca18e1d9cbac26c488e2474a3c39d506
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2023-08-14 09:22:12 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const n = expr.length / 10;
+    let sym = [];
+    for (let i = 0; i < n; i++) {
+      sym[i] = expr.slice((i * 10), ((i + 1) * 10));
+    }
+  
+    return sym.map(item => {
+      if (item === '**********') {
+        return ' ';
+      } else {
+        let char = '';
+        for (let i = 0; i < 5; i++) {
+          const binNum = item.slice((i * 2), ((i + 1) * 2));
+          if (binNum === '00') {
+            char += '';
+          } else if (binNum === '10') {
+            char += '.';
+          } else if (binNum === '11') {
+            char += '-';
+          }
+        }
+  
+        let result = '';
+        for (let key in MORSE_TABLE) {
+          if (key === char) {
+            result += MORSE_TABLE[key]
+          }
+        }
+        return result.toString();
+      }
+    }).join('');
 }
 
 module.exports = {
